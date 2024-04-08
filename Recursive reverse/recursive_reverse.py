aceca9546a6415f335ef1cc8d71b8f4d02d93837
Sofiia Popeniuk <146963804+SofiiaPop@users.noreply.github.com>
2024-04-08 16:54:26 +0300

    Create recursive_reverse.py

---
+++ Recursive reverse/recursive_reverse.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    prev = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    
+    return prev
