2ebed2b53350984a8358c31365debce31cb9f961
Sofiia Popeniuk <146963804+SofiiaPop@users.noreply.github.com>
2024-04-08 16:55:28 +0300

    Create swap_node_pairs.py

---
+++ Swap Node Pairs In Linked List/swap_node_pairs.py
@@ -0,0 +1,17 @@
+def swap_pairs(head):
+    dummy = Node(0)
+    dummy.next = head
+    prev = dummy
+
+    while head and head.next:
+        first_node = head
+        second_node = head.next
+
+        prev.next = second_node
+        first_node.next = second_node.next
+        second_node.next = first_node
+
+        prev = first_node
+        head = first_node.next
+
+    return dummy.next
