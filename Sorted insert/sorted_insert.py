204441a7946d9bc5859f88ec0910f6e0f8fa3b32
Sofiia Popeniuk <146963804+SofiiaPop@users.noreply.github.com>
2024-04-08 16:49:13 +0300

    Create sorted_insert.py

---
+++ Sorted insert/sorted_insert.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data, next=None): 
+        self.data = data
+        self.next = next
+
+def sorted_insert(head, data):
+    new_node = Node(data)
+    if head is None or head.data > data:
+        new_node.next = head
+        return new_node
+
+    current = head
+    while current.next is not None and current.next.data < data:
+        current = current.next
+    
+    new_node.next = current.next
+    current.next = new_node
+
+    return head
+
+def build_one_two_three():
+    return Node(1, Node(2, Node(3)))
